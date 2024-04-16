fdb89312d6a3b32c50cc9326006f5816827a869f
larry.ramjutton <larry.ramjutton@ordina.be>
2024-04-16 13:42:28 +0200

    first commit

--- app/config/environment.d.ts
+++ app/config/environment.d.ts
@@ -1,12 +1,12 @@
 /**
  * Type declarations for
- *    import config from 'intro/config/environment'
+ *    import config from 'my-app/config/environment'
  */
 declare const config: {
   environment: string;
   modulePrefix: string;
   podModulePrefix: string;
-  locationType: 'history' | 'hash' | 'none';
+  locationType: 'history' | 'hash' | 'none' | 'auto';
   rootURL: string;
   APP: Record<string, unknown>;
 };
